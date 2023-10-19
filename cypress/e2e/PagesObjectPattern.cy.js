5f9359fa4a14d6468361baeca1b39edddb49f49b
Artur Wiśniewski <a.wisniewski0@gmail.com>
2023-10-19 16:14:09 +0200

    First commit

---
+++ cypress/e2e/PagesObjectPattern.cy.js
@@ -0,0 +1,30 @@
+import LoginPage from "../fixtures/Login";
+import HomePage from "../fixtures/HomePage";
+
+describe("Test 1", () => {
+  it("Logowanie użytkownika user888@gmail.com", () => {
+    LoginPage.visit();
+    LoginPage.fillEmail("user888@gmail.com");
+    LoginPage.fillPassword("1234567890");
+    LoginPage.clickLogInButton();
+
+    HomePage.openUserMenu();
+    HomePage.clickLogOutButton();
+
+    LoginPage.verifyUrl();
+  });
+});
+
+describe("Test 2", () => {
+  it("Logowanie użytkownika testowyqa@qa.team", () => {
+    LoginPage.visit();
+    LoginPage.fillEmail("testowyqa@qa.team");
+    LoginPage.fillPassword("QA!automation-1");
+    LoginPage.clickLogInButton();
+
+    HomePage.openUserMenu();
+    HomePage.clickLogOutButton();
+
+    LoginPage.verifyUrl();
+  });
+});
